6ab59e31d70e4c0a70c7a4fcfbd68623225e1504
Neakita <thekatterdev@gmail.com>
2025-03-03 13:55:52 +0500

    Add missing ActionDisposable

---
+++ Vibrance/ActionDisposable.cs
@@ -0,0 +1,16 @@
+namespace Vibrance;
+
+internal sealed class ActionDisposable : IDisposable
+{
+	public ActionDisposable(Action action)
+	{
+		_action = action;
+	}
+
+	public void Dispose()
+	{
+		_action();
+	}
+
+	private readonly Action _action;
+}
