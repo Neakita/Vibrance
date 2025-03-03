a8c7e70cba63bed2f0285b7f8d860104159aa573
Neakita <thekatterdev@gmail.com>
2025-03-03 13:54:40 +0500

    Make Transform proper extension method with this modifier

--- Vibrance/ObservableChangesExtensions.cs
+++ Vibrance/ObservableChangesExtensions.cs
@@ -5,7 +5,7 @@ namespace Vibrance;
 public static class ObservableChangesExtensions
 {
 	public static IObservable<Change<TDestination>> Transform<TSource, TDestination>(
-		IObservable<Change<TSource>> source,
+		this IObservable<Change<TSource>> source,
 		Func<TSource, TDestination> selector)
 	{
 		return new ChangesTransformer<TSource, TDestination>(source, selector);
