ddac643c159379311726344f546b65a7146cbff3
Neakita <thekatterdev@gmail.com>
2025-03-06 17:52:51 +0500

    Add benchmark project

---
+++ Vibrance.Benchmarks/TransformBenchmark.cs
@@ -0,0 +1,57 @@
+using BenchmarkDotNet.Attributes;
+using DynamicData;
+
+namespace Vibrance.Benchmarks;
+
+public class TransformBenchmark
+{
+	[Params(10, 1_000, 10_000, 100_000)]
+	public int Quantity;
+
+	[GlobalSetup]
+	public void Setup()
+	{
+		_itemsToAdd = Enumerable.Range(0, Quantity).ToList();
+	}
+
+	[IterationSetup(Target = nameof(VibranceTransformAdd))]
+	public void VibranceSetup()
+	{
+		_vibranceSubscription?.Dispose();
+		_vibranceSourceList = new SourceList<int>();
+		_vibranceSubscription = _vibranceSourceList
+			.Transform(number => number.ToString())
+			.ToObservableList(out _);
+	}
+
+	[Benchmark]
+	public void VibranceTransformAdd()
+	{
+		_vibranceSourceList!.AddRange(_itemsToAdd!);
+	}
+
+	[IterationSetup(Target = nameof(DynamicDataTransformAdd))]
+	public void DynamicDataSetup()
+	{
+		_dynamicDataSubscription?.Dispose();
+		_dynamicDataSourceList?.Dispose();
+		_dynamicDataSourceList = new DynamicData.SourceList<int>();
+		_dynamicDataSubscription = _dynamicDataSourceList
+			.Connect()
+			.Transform(number => number.ToString())
+			.Bind(out _)
+			.Subscribe();
+	}
+
+	[Benchmark]
+	public void DynamicDataTransformAdd()
+	{
+		_dynamicDataSourceList!.AddRange(_itemsToAdd!);
+	}
+
+	private List<int>? _itemsToAdd;
+	private SourceList<int>? _vibranceSourceList;
+	private IDisposable? _vibranceSubscription;
+	private DynamicData.SourceList<int>? _dynamicDataSourceList;
+	private IDisposable? _dynamicDataSubscription;
+}
