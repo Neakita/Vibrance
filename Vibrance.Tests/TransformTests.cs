7374f7d6a9b27d285a550a2af4f5bb87b6719494
Neakita <thekatterdev@gmail.com>
2025-03-07 16:21:15 +0500

    Add VerifyDataIntegrity and new tests to TransformTests

--- Vibrance.Tests/TransformTests.cs
+++ Vibrance.Tests/TransformTests.cs
@@ -9,26 +9,70 @@ public sealed class TransformTests
 	[Fact]
 	public void ShouldObserveInitialItems()
 	{
-		SourceList<int> list = [1, 2, 3];
-		using var observer = list.Transform(number => number.ToString()).ObserveChanges();
+		SourceList<int> source = [1, 2, 3];
+		using var observer = source.Transform(number => number.ToString()).ObserveChanges();
 		observer.LastObservedValue.NewItems.Should().ContainInOrder("1", "2", "3");
+		VerifyDataIntegrity(source, NumberToString, observer);
 	}
 
 	[Fact]
 	public void ShouldObserveReset()
 	{
-		SourceList<int> list = [1, 2, 3];
-		using var observer = list.Transform(number => number.ToString()).ObserveChanges();
-		list.Clear();
+		SourceList<int> source = [1, 2, 3];
+		using var observer = source.Transform(number => number.ToString()).ObserveChanges();
+		source.ReplaceAll(4, 5, 6);
 		observer.LastObservedValue.Reset.Should().BeTrue();
+		observer.LastObservedValue.NewItems.Should().ContainInOrder("4", "5", "6");
+		observer.LastObservedValue.OldItems.Should().BeEmpty();
+		VerifyDataIntegrity(source, NumberToString, observer);
 	}
 
 	[Fact]
-	public void ShouldObserveRemovedItems()
+	public void ShouldObserveOldItems()
 	{
-		SourceList<int> list = [1, 2, 3];
-		using var observer = list.Transform(number => number.ToString()).ObserveChanges();
-		list.RemoveRange(0, 2);
+		SourceList<int> source = [1, 2, 3];
+		using var observer = source.Transform(number => number.ToString()).ObserveChanges();
+		source.RemoveRange(0, 2);
 		observer.LastObservedValue.OldItems.Should().ContainInOrder("1", "2");
+		VerifyDataIntegrity(source, NumberToString, observer);
+	}
+
+	[Fact]
+	public void ShouldObserveNewItems()
+	{
+		SourceList<int> source = [1, 2, 3];
+		using var observer = source.Transform(number => number.ToString()).ObserveChanges();
+		source.InsertRange(1, 4, 5, 6);
+		observer.LastObservedValue.NewItems.Should().ContainInOrder("4", "5", "6");
+		observer.LastObservedValue.NewItems.StartIndex.Should().Be(1);
+		VerifyDataIntegrity(source, NumberToString, observer);
+	}
+
+	[Fact]
+	public void ShouldObserveMove()
+	{
+		SourceList<int> source = [1, 2, 3];
+		using var observer = source.Transform(NumberToString).ObserveChanges();
+		source.MoveRange(0, 2, 1);
+		var observation = observer.LastObservedValue;
+		observation.OldItems.StartIndex.Should().Be(0);
+		observation.OldItems.Should().ContainInOrder("1", "2");
+		observation.NewItems.StartIndex.Should().Be(1);
+		observation.NewItems.Should().ContainInOrder("1", "2");
+		VerifyDataIntegrity(source, NumberToString, observer);
+	}
+
+	private static string NumberToString(int number)
+	{
+		return number.ToString();
+	}
+
+	private static void VerifyDataIntegrity<TSource, TDestination>(
+		IReadOnlyList<TSource> source,
+		Func<TSource, TDestination> selector,
+		RecordingObserver<Change<TDestination>> observer)
+	{
+		var transformed = ((InnerListProvider<TDestination>)observer.Subscription).Inner;
+		transformed.Should().ContainInOrder(source.Select(selector));
 	}
 }
